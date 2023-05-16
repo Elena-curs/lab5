46c0b853304e9228e3449cd8625dcad79a6a890a
Elena <Lena.Drebezgova.ru@yandex.ru>
2023-05-16 10:37:11 +0300

    Calculator

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the calculator!");
+
+            Console.WriteLine("Enter the first number:");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            double num2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the operator (+, -, *, /):");
+            char op = char.Parse(Console.ReadLine());
+
+            double result = 0;
+
+            switch (op)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+
+                case '-':
+                    result = num1 - num2;
+                    break;
+
+                case '*':
+                    result = num1 * num2;
+                    break;
+
+                case '/':
+                    result = num1 / num2;
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid operator");
+                    break;
+            }
+
+            Console.WriteLine("The result is: " + result);
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
